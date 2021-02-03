98d88e7494293daf4632d3c1d41667ce209c2c19
Jenny Tan <tanjenny.yo@gmail.com>
2021-02-03 19:21:02 +0000

    finished parsing all job details, incl posting date conversion

--- linkedin.py
+++ linkedin.py
@@ -1,13 +1,15 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 
-# url = 'https://www.indeed.com/jobs?q=python&l=new+york'
-url = 'https://www.linkedin.com/jobs/view/2165533703/?alternateChannel=search&refId=oZlHex62ZpQS%2BjOjDjDUBg%3D%3D&trackingId=hLNFWOL4P%2F21pZZVdkxyrA%3D%3D'
+weeks_url = 'https://www.linkedin.com/jobs/view/2165533703/?alternateChannel=search&refId=oZlHex62ZpQS%2BjOjDjDUBg%3D%3D&trackingId=hLNFWOL4P%2F21pZZVdkxyrA%3D%3D'
 # url = 'https://www.linkedin.com/jobs/view/2406107518/?refId=CAHwyJXAZadoE9o%2FVbXbfw%3D%3D&trackingId=M6KQvLu%2FdNPuEHd1Ky%2FINw%3D%3D'
 
-page = requests.get(url)
+hours_url = 'https://www.linkedin.com/jobs/view/2393605490/?alternateChannel=search&refId=pru82Hpg9w6tQ5Fw0jgnTw%3D%3D&trackingId=EVioOyXIQq0lsF%2BVoRc6Mw%3D%3D'
+days_url = 'https://www.linkedin.com/jobs/view/2386792270/?alternateChannel=search&refId=pru82Hpg9w6tQ5Fw0jgnTw%3D%3D&trackingId=pzQzqtJZVkA0bpd70RQZQA%3D%3D&trk=flagship3_search_srp_jobs'
+
+page = requests.get(hours_url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -28,10 +30,30 @@ seniority_level = soup.find(
 employment_type = soup.find(
     'ul', class_='job-criteria__list').find_all('li', class_='job-criteria__item')[1].find('span', class_='job-criteria__text job-criteria__text--criteria').text
 
-posting_date = soup.find(
-    'span', class_="topcard__flavor--metadata posted-time-ago__text").text
 
-# TODO: use datetime to calculate actual calendar date that it was posted
+def find_interval():
+    interval = soup.find(
+        'span', class_="topcard__flavor--metadata posted-time-ago__text posted-time-ago__text--new")
+    if not interval:
+        interval = soup.find('span', class_="topcard__flavor--metadata posted-time-ago__text")
+    return interval.text
+
+
+interval = find_interval()
+
+#turned string into list [qt, unit, 'ago']
+interval_list = interval.split()
+unit = interval_list[1]
+quantity = int(interval_list[0])
+
+if unit == "days":
+    delta = timedelta(days=quantity)
+elif unit == "weeks":
+    delta = timedelta(weeks=quantity)
+elif unit == 'hours':
+    delta = timedelta(hours=quantity)
+
+posting_date = (datetime.now() - delta).strftime("%m-%d-%Y")
 
 print("Job Title: ", job_title)
 print("Company: ", company)
@@ -40,6 +62,3 @@ print("Location: ", location)
 print("Seniority Level: ", seniority_level)
 print("Employment type: ", employment_type)
 print("Posting Date: ", posting_date)
-
-date = posting_date.strftime("%m-%d-%Y")
-print(date)
